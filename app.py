ccfdca3941225efd7b5817a3f17495e87056e9bb
Joel-Garcia-Martinez <1719110587@utectulancingo.edu.mx>
2022-04-21 20:21:13 +0000

    creacion de carpetas

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ import json #importa el archivo json
 
 urls =(
     "/", "mvc.controllers.public.index.Index",
+    "/", "mvc.views.public.index.Index",
     "/sensores", "mvc.controllers.public.sensores.Sensores"
 )
 
 app = web.application(urls, globals())
+app = web.application(urls, globals()) #toma las urls de arriba e inicia con la ultima linea de este codigo
+render = web.template.render('views') #indica un conteo de las veces que se visita la pagina 
+firebase = pyrebase.initialize_app(token.firebaseConfig)
+auth = firebase.auth()
 
 class Sensores:
     def GET (self):
